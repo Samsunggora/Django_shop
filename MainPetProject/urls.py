70bb53483b1c18389c5c2ce9c5f55b52f75c893d
Samsunggora <Samsunggora@gmail.com>
2021-12-25 17:33:55 +0100

    add product in cart, fix little bugs

--- MainPetProject/urls.py
+++ MainPetProject/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
+    re_path(r'^cart/', include(('cart.urls', 'cart'), namespace='cart')),
     re_path(r'^', include(('shop.urls', 'shop'), namespace='shop'))
 ]
 
